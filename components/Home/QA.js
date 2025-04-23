623e133d7ba311b9272aee1d88b55c20448f98b2
Matthew <matthewjwright07@gmail.com>
2025-04-22 21:29:26 -0500

    Color changes

--- components/Home/QA.js
+++ components/Home/QA.js
@@ -1,7 +1,7 @@
 export default function QA(){
     return (
         <div className="px-8 py-2">
-            <div className="px-8 bg-primary rounded-lg py-8 flex flex-col gap-2">
+            <div className="px-8 bg-dark rounded-lg py-8 flex flex-col gap-2">
                 <h1 className="text-3xl font-bold text-white">Q&A</h1>
                 <div className="border border-white text-white p-4 rounded-lg flex justify-between">
                     <h1>What do you do?</h1>
--- components/Home/Start.js
+++ components/Home/Start.js
@@ -1,7 +1,7 @@
 export default function Start(){
     return (
         <div className="px-8 py-2">
-            <div className="flex flex-col justify-center items-center rounded-lg bg-dark text-white py-10 gap-4">
+            <div className="flex flex-col justify-center items-center rounded-lg bg-primary text-white py-10 gap-4">
                 <h1 className="text-3xl font-bold">Lets Get Started</h1>
                 <a href="" className="h-12 w-40 border border-white flex items-center justify-center rounded-lg text-white hover:scale-110 transition duration-500">Start</a>
             </div>
